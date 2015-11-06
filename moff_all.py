79254b7f19a9f4d17ca061dc87ba3aac087b2fd0
Compomics User <compomics@codeb.UGent.be>
2015-11-06 16:26:11 +0100

    tested all input ok

--- moff_all.py
+++ moff_all.py
@@ -21,9 +21,9 @@ parser.add_argument('--inputF', dest='loc_in', action='store', help='specify the
 
 parser.add_argument('--sample', dest='sample', action='store', help='specify witch replicated to use for mbr reg_exp are valid ', required=False)
 
-parser.add_argument('--ext', dest='ext', action='store', default='.txt', help='specify the file extentention of the input like ', required=False)
+parser.add_argument('--ext', dest='ext', action='store', default='txt', help='specify the file extentention of the input like ', required=False)
 
-parser.add_argument('--log_file_name', dest='log_label', action='store', help='a label name to use for the log file', required=False)
+parser.add_argument('--log_file_name', dest='log_label', action='store', default='moFF', help='a label name to use for the log file', required=False)
 
 parser.add_argument ('--filt_width', dest='w_filt', action='store',default=2,help='width value of the filter  k * mean(Dist_Malahobis)', required=False  )
 
@@ -35,7 +35,7 @@ parser.add_argument('--weight_comb', dest='w_comb', action='store',default=0,hel
 
 parser.add_argument('--tol', dest='toll',action='store',type= float,help='specify the tollerance  parameter in ppm', required=True)
 
-parser.add_argument('--rt_w', dest='rt_window', action='store',type= float, default=3,help='specify rt window for xic (minute). Default value is 3 min', required=True)
+parser.add_argument('--rt_w', dest='rt_window', action='store',type= float, default=3,help='specify rt window for xic (minute). Default value is 3 min', required=False)
 
 parser.add_argument('--rt_p', dest='rt_p_window', action='store',type= float, default=0.1,help='specify the time windows for the peak ( minute). Default value is 0.1 ', required=False)
 
@@ -49,14 +49,16 @@ parser.add_argument('--output_folder', dest='loc_out', action='store', default='
 args = parser.parse_args()
 
 
+print 'Matching between run module (mbr)'
+
 moff_mbr.run_mbr(args)
 
 
 folder = args.loc_in + 'mbr_output'
 #os.chdir(folder)
+print 'Apex module '
 for file in glob.glob(folder+"/*.txt"):
         file_name =file
-	print 'Starting apex ',file_name,'............',
 	tol= args.toll
         h_rt_w = args.rt_window
         s_w= args.rt_p_window
@@ -64,8 +66,7 @@ for file in glob.glob(folder+"/*.txt"):
         loc_raw = args.raw
         loc_output = args.loc_out
 	moff.run_apex(  file_name,tol,h_rt_w,s_w,s_w_match,loc_raw,loc_output )
-	print 'Ended '
-
+	print '-------------     -------------'
 
 #moff.run_apex(args)
  
