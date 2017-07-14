1b30a56882a261030a5757695eee04ed087be4a7
Johannes Griss <jgriss8@yahoo.com>
2017-07-14 11:51:33 +0200

    Fixed bug in the way the txic path was created. Set "txic" as executable.

--- moff.py
+++ moff.py
@@ -209,8 +209,6 @@ def apex_multithr(data_ms2,name_file, raw_name, tol, h_rt_w, s_w, s_w_match, loc
             flag_mzml = True
             run_temp = pymzml.run.Reader(raw_name)
 
-
-    
     if os.path.isfile(loc):
         log.info('raw file exist')
     else:
@@ -291,8 +289,7 @@ def apex_multithr(data_ms2,name_file, raw_name, tol, h_rt_w, s_w, s_w_match, loc
                     os.path.join('folder_name', 'file_name')
                     args_txic = shlex.split(os.path.join(moff_path, "txic.exe") + " " + mz_opt + " -tol=" + str(tol) + " -t " + str( time_w - h_rt_w) + " -t " + str(time_w + h_rt_w) + " " + loc, posix=False)
                 else:
-                    args_txic = shlex.split(TXIC_PATH + "txic " + mz_opt + " -tol=" + str(tol) + " -t " + str(time_w - h_rt_w) + " -t " + str(time_w + h_rt_w) + " " + loc )
-
+                    args_txic = shlex.split(os.path.join(moff_path, "txic") + " " + mz_opt + " -tol=" + str(tol) + " -t " + str(time_w - h_rt_w) + " -t " + str(time_w + h_rt_w) + " " + loc )
                 
                 p = subprocess.Popen(args_txic, stdout=subprocess.PIPE)
                 output, err = p.communicate()
